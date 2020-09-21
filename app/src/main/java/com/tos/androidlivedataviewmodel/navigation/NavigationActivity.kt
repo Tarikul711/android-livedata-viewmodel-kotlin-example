9959d6bdec106b9e66a4089055deed76bdfb60e7
Tarikul711 <Tarikul711@gmail.com>
2020-09-21 20:29:58 +0600

    Add Menu

--- app/src/main/java/com/tos/androidlivedataviewmodel/navigation/NavigationActivity.kt
+++ app/src/main/java/com/tos/androidlivedataviewmodel/navigation/NavigationActivity.kt
@@ -2,12 +2,27 @@ package com.tos.androidlivedataviewmodel.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.tos.androidlivedataviewmodel.R
+import kotlinx.android.synthetic.main.activity_navigation.*
 
 class NavigationActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+//    private lateinit var drawerLayout:DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+        navController = findNavController(R.id.fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        navigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,drawerLayout)
     }
 }
