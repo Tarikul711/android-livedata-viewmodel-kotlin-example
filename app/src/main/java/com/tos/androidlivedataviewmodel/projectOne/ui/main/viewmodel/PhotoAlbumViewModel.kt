01e1bba8dbfce8442477cc27e4569b3474ecd36b
Tarikul711 <Tarikul711@gmail.com>
2020-09-23 23:16:07 +0600

    Modify the PhotoAlbumViewModel.kt implement the kotlin coroutine flow

--- app/src/main/java/com/tos/androidlivedataviewmodel/projectOne/ui/main/viewmodel/PhotoAlbumViewModel.kt
+++ app/src/main/java/com/tos/androidlivedataviewmodel/projectOne/ui/main/viewmodel/PhotoAlbumViewModel.kt
@@ -3,11 +3,13 @@ package com.tos.androidlivedataviewmodel.projectOne.ui.main.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.tos.androidlivedataviewmodel.projectOne.data.model.PhotoModel
 import com.tos.androidlivedataviewmodel.projectOne.data.repository.PhotoAlbumRepository
 import com.tos.androidlivedataviewmodel.projectOne.utils.Resource
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 /**
  *Created by tarikul on 16/9/20
@@ -17,27 +19,19 @@ class PhotoAlbumViewModel(var photoAlbumRepository: PhotoAlbumRepository) : View
 
     private var photoAlbum = MutableLiveData<Resource<List<PhotoModel>>>();
 
-    fun getAllPhotos() = liveData {
-        apiHelper.getUsers()
-            .catch { e ->
-                users.postValue(Resource.error(e.toString(), null))
-            }
-            .collect {
-                users.postValue(Resource.success(it))
-            }
-
+    fun getAllPhotos() = viewModelScope.launch {
         photoAlbum.postValue(Resource.loading(data = null))
         photoAlbumRepository.getPhotoAlbum()
-            .catch { photoAlbum.postValue(Resource.error(null,"Error Occurred")) }
+            .catch { photoAlbum.postValue(Resource.error(null, "Error Occurred")) }
             .collect { photoAlbum.postValue(Resource.success(it)) }
 
 
-       /* emit(Resource.loading(data = null))
-        try {
-            emit(Resource.success(data = photoAlbumRepository.getPhotoAlbum()))
-        } catch (exception: Exception) {
-            emit(Resource.error(data = null, message = exception.message ?: "Error Occurred"))
-        }*/
+        /* emit(Resource.loading(data = null))
+         try {
+             emit(Resource.success(data = photoAlbumRepository.getPhotoAlbum()))
+         } catch (exception: Exception) {
+             emit(Resource.error(data = null, message = exception.message ?: "Error Occurred"))
+         }*/
     }
 
     private fun getPhotoData(): MutableLiveData<Resource<List<PhotoModel>>> {
