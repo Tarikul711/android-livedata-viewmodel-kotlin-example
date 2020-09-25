0e4a47fdbf8be4ab53d36d3fce0f8c0d811227cc
Tarikul711 <Tarikul711@gmail.com>
2020-09-25 19:35:50 +0600

    Add a new activity

---
+++ app/src/main/java/com/tos/androidlivedataviewmodel/testingPart/ConstraintActivity.kt
@@ -0,0 +1,12 @@
+package com.tos.androidlivedataviewmodel.testingPart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tos.androidlivedataviewmodel.R
+
+class ConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
